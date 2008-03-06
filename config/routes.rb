946bfcd2c75c38762c234390149c36b099873ed0
Chris Scharf <scharfie@gmail.com>
2008-03-06 16:19:04 -0500

    Added restful auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :users
+
+  map.resource :session
+
   map.root :controller => 'chat'
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
