f2b916ca0d7f6ff5a154a9f66439291d418c9ecb
Chris Scharf <scharfie@gmail.com>
2008-03-08 13:40:51 -0500

    Added GabberStatus for handling status codes

--- app/models/gabber.rb
+++ app/models/gabber.rb
@@ -1,24 +1,72 @@
 class Gabber < ActiveRecord::Base
+  delegate :online?, :away?, :offline?, :to => :status
   belongs_to :user
   
-  ONLINE = 0
-  AWAY   = 1
+  def self.gabbers
+    find :all, :include => :user
+  end
   
   def self.grab(user)
     gabber = find_or_initialize_by_user_id(User === user ? user.id : user)
   end
   
-  def self.update_status(user_id, status=ONLINE, message=nil)
-    gabber = grab(user_id)
-    gabber.online!    if status == ONLINE
-    gabber.away!(msg) if status == AWAY
+  def status
+    @gs ||= GabberStatus.new(self[:status])
+  end
+  
+  def offline!
+    update_attributes :status => GabberStatus::OFFLINE, :message => nil
   end
   
   def online!
-    update_attributes :status => ONLINE, :message => nil
+    update_attributes :status => GabberStatus::ONLINE, :message => nil
   end
   
   def away!(msg=nil)
-    update_attributes :status => AWAY, :message => msg
+    update_attributes :status => GabberStatus::AWAY, :message => msg
+  end
+end
+
+class GabberStatus
+  OFFLINE = 0
+  ONLINE  = 1
+  AWAY    = 2
+    
+  attr_accessor :status
+  
+  def initialize(status)
+    @status = status
+  end
+  
+  def to_i
+    @status
+  end
+  
+  def to_s
+    case @status
+    when ONLINE
+      'online'
+    when OFFLINE
+      'offline'  
+    when AWAY
+      'away'  
+    end
+  end
+  
+  def online?
+    status == ONLINE
+  end
+  
+  def offline?
+    status == OFFLINE
+  end
+  
+  def away?
+    status == AWAY
+  end  
+  
+  def ==(other)
+    return to_i == other if Integer === other
+    return to_s == other if String  === other
   end
 end
